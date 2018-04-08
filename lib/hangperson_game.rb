01ca08b58ad117e76474febc8418daed79dccc62
Cory Eighan <zephyr834@gmail.com>
2018-04-08 16:39:57 -0700

    Hangman Part 1 complete.
    New game init complete. Correctly guessing test complete. Incorrect guessing tests complete.
    Same letter tests pass.
    Invalid character guess tests pass.
    Displayed word with guesses completed.
    Game status complete.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,11 +5,43 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    raise ArgumentError.new('Invalid guess. Guess a letter.') unless letter =~ /[[:alpha:]]/
+
+    letter.downcase!
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    if @word.include?(letter) 
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    return true
+  end
+
+  def word_with_guesses
+    result = @guesses.empty? ? @word.gsub(/./, '-') : @word.gsub(/[^#{@guesses}]/, '-')
+  end
+
+  def check_win_or_lose
+    wordStatus = word_with_guesses
+    if wordStatus.eql?(@word)
+      status = :win
+    elsif @wrong_guesses.length > 6 #user gets 7 guesses
+      status = :lose
+    else
+      status = :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
